314ee4ca5f52304c89fac71b8f293774341d6278
Daniel Pett <dpett@britishmuseum.org>
2017-09-01 10:35:49 +0100

    Added copyright and OrcID badge to readme template

--- make3d.py
+++ make3d.py
@@ -42,12 +42,14 @@ def write_readme(project, root_dir):
 def get_readme_text(project):
     readme_text = """
         [![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](http://creativecommons.org/licenses/by-sa/4.0/) 
+        [![ORCiD](https://img.shields.io/badge/ORCiD-0000--0002--0246--2335-green.svg)](http://orcid.org/0000-0002-0246-2335)
         # {project}
         3D data for recreation of a British Museum object.
         # LICENSE
         The contents of this repository are licensed under CC-BY-NC-SA
         # Credits
         Photographs and models by {author} <{author_email}>, Digital Humanities Lead, British Museum
+        Copyright Trustees of the British Museum
     """.format(
         project=project,
         license=license,
